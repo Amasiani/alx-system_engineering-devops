ce3537ff263165a18a7e3228436c3e406bc922e8
amasiani <amasianiemeka@gmail.com>
2023-11-29 19:08:09 -0800

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 #!/usr/bin/pup
 # installing a specific version of flask 2.1.0
 
-package{'Werkzeug':
- ensure => '2.2.2',
- provider => 'pip3',
-}
-
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
