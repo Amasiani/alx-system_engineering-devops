939f95f502e7bdaa44b30aa5346c9bece1e91334
Amasiani <amasianiemeka@gmail.com>
2024-05-08 05:57:17 +0100

    corrected requests

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Function to query a list of all hot posts on a given Reddit subreddit."""
-import request
+import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
